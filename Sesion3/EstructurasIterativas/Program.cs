a53217377e213e8f371ee849b6f717dc68f6f16c
Pool Reategui <reateguiaguilarpoolalexander@gmail.com>
2025-12-16 00:42:20 -0500

    Estructura while Finalizada

--- Sesion3/EstructurasIterativas/Program.cs
+++ Sesion3/EstructurasIterativas/Program.cs
@@ -4,6 +4,10 @@ int opcion = 0;
 
 while (opcion!= 7)
 {
+    Console.WriteLine("Ingrese numero a operar");
+    double numero1 = double.Parse(Console.ReadLine());
+    double numero2 = double.Parse(Console.ReadLine());
+    double resultado = 0;
     Console.WriteLine("Calculadora:");
     Console.WriteLine("1. Sumar");
     Console.WriteLine("2. Restar");
@@ -15,13 +19,52 @@ while (opcion!= 7)
     Console.WriteLine("Seleccione una opcion (1-7):");
     opcion = int.Parse(Console.ReadLine());
 
-    if (opcion == 1)
+    switch(opcion)
     {
-        Console.WriteLine("Sumando");
-    }
-    if (opcion == 2)
-        {
-        Console.WriteLine("Restando");
+        case 1:
+            resultado = numero1 + numero2;
+            Console.WriteLine("Resultado: " + resultado);
+            break;
+        case 2:
+            resultado = numero1 - numero2;
+            Console.WriteLine("Resultado: " + resultado);
+            break;
+        case 3:
+            resultado = numero1 * numero2;
+            Console.WriteLine("Resultado: " + resultado);
+            break;
+        case 4:
+            if (numero2 != 0)
+            {
+                resultado = numero1 / numero2;
+                Console.WriteLine("Resultado: " + resultado);
+            }
+            else
+            {
+                Console.WriteLine("Error: Division por cero");
+            }
+            break;
+        case 5:
+            resultado = Math.Pow(numero1, numero2);
+            Console.WriteLine("Resultado: " + resultado);
+            break;
+        case 6:
+            if (numero1 >= 0)
+            {
+                resultado = Math.Sqrt(numero1);
+                Console.WriteLine("Resultado: " + resultado);
+            }
+            else
+            {
+                Console.WriteLine("Error: Raiz cuadrada de un numero negativo");
+            }
+            break;
+        case 7:
+            Console.WriteLine("Saliendo del programa...");
+            break;
+        default:
+            Console.WriteLine("Opcion invalida. Intente de nuevo.");
+            break;
     }
 }
 Console.WriteLine("Fin del programa");
