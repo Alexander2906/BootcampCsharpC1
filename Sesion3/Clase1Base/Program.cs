684b0c92a1275f2614328cb1b45bc9d901c80ea8
Pool Reategui <reateguiaguilarpoolalexander@gmail.com>
2025-12-16 00:01:07 -0500

    actualizacion

--- Sesion3/Clase1Base/Program.cs
+++ Sesion3/Clase1Base/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hola, Alumnos");
+// variables
+int edad = 20;
+long altura = 180L;
+double dinero = 1500.50;
+float peso = 70.5f;
+decimal saldo = 10000.75m;
+String nombre = "pedro";
+char letra = 'A';
+bool tieneGorro = true;
+Console.WriteLine("Escribe tu nombre:");
+nombre = Console.ReadLine();
+Console.WriteLine("Mi nombre es, " + nombre);
+Console.WriteLine($"Mi altura es, {altura}");
+Console.WriteLine("Ecribe tu dinero");
+dinero = double.Parse(Console.ReadLine());
+Console.WriteLine($"Tu dinero es, {dinero}");
